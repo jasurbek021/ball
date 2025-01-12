b17bc217935f19137d0700e2def243e91af9e370
jasur@gmail.com <jasur@gmail.com>
2025-01-12 12:19:50 +0500

    m

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const field = document.getElementById('field');
-const ball = document.getElementById('ball');
+const field = document.getElementById("field");
+const ball = document.getElementById("ball");
 
-field.addEventListener('click', function (event) {
-  const x = event.clientX;
-  const y = event.clientY; 
-  ball.style.transform = `translate(${x - 25}px, ${y - 25}px)`;
-});     
+field.addEventListener("click", function (event) {
+  const field = document.getElementById("field");
+  const ball = document.getElementById("ball");
+
+  field.addEventListener("click", function (event) {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    ball.style.left = `${x - 25}px`;
+    ball.style.top = `${y - 25}px`;
+
+    ball.style.animation = "none";
+    void ball.offsetWidth;
+    ball.style.animation = "spin 3s ease-out";
+  });
+});
